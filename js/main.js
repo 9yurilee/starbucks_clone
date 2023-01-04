dbf2250d6b1c701831ba2555ff009659fe1089f3
이규리 <degurrrrrr@gmail.com>
2023-01-04 09:07:27 +0900

    💄 style: 헤더 부분 css 작업 완료 및 main-menu 작업중

---
+++ js/main.js
@@ -0,0 +1,19 @@
+const searchEl = document.querySelector(".search");
+const searchInputEl = searchEl.querySelector("input");
+
+searchEl.addEventListener("click", function () {
+  searchInputEl.focus();
+});
+
+searchInputEl.addEventListener("focus", function () {
+  searchEl.classList.add("focused"); // focused라는 클래스 추가
+  searchInputEl.setAttribute("placeholder", "통합검색"); // html 속성 지정할 때
+});
+
+searchInputEl.addEventListener("blur", function () {
+  searchEl.classList.remove("focused");
+  searchInputEl.setAttribute("placeholder", ""); // html 속성 지정할 때
+});
+
+const title = document.querySelector(".item-title");
+const bottom = document.querySelector(".item-contents");
