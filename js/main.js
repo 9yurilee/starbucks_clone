6215b0c5673919a3a81d8b8ce5d449936fca27c9
이규리 <degurrrrrr@gmail.com>
2023-01-05 11:56:13 +0900

    ✨ feat: 슬라이드 등 추가

--- js/main.js
+++ js/main.js
@@ -20,7 +20,6 @@ const badgeEl = document.querySelector("header .badges");
 window.addEventListener(
   "scroll",
   _.throttle(function () {
-    console.log(window.scrollY);
     if (window.scrollY > 500) {
       gsap.to(badgeEl, 0.6, {
         opacity: 0,
@@ -34,3 +33,76 @@ window.addEventListener(
     }
   }, 300)
 );
+
+// VISUAL 순차적으로 나타나기
+const fadeEls = document.querySelectorAll(".visual .fade-in");
+
+fadeEls.forEach(function (fadeEl, index) {
+  gsap.to(fadeEl, 1, {
+    delay: (index + 1) * 0.7, //순차적으로
+    opacity: 1 //CSS에서 opacity : 0인걸 1로 바꾸는 것!
+  });
+});
+
+// 공지사항 슬라이드
+// new Swiper(선택자, 옵션)
+new Swiper(".notice-line .swiper-container", {
+  direction: "vertical",
+  autoplay: true,
+  loop: true
+});
+
+new Swiper(".promotion .swiper-container", {
+  slidesPerView: 3, // 한 번에 보여줄 슬라이드 개수
+  spaceBetween: 10, // 슬라이드 사이 여백(px)
+  centeredSlides: true, // 1번 슬라이드가 가운데
+  loop: true,
+  autoplay: {
+    delay: 5000 // 5s
+  }, // 객체면 autoplay 추가 옵션 기재 가능
+  pagination: {
+    el: ".promotion .swiper-pagination", // 페이지 번호 요소 선택자
+    clickable: true // 페이지네이션 클릭 가능 여부
+  },
+  navigation: {
+    prevEl: ".promotion .swiper-prev",
+    nextEl: ".promotion .swiper-next"
+  }
+});
+
+// 프로모션 토글 기능
+const promotionEl = document.querySelector(".promotion");
+const promotionToggleBtn = document.querySelector(".toggle-promotion");
+
+let isHidePromotion = false;
+
+promotionToggleBtn.addEventListener("click", function () {
+  isHidePromotion = !isHidePromotion;
+  if (isHidePromotion) {
+    promotionEl.classList.add("hide");
+  } else {
+    promotionEl.classList.remove("hide");
+  }
+});
+
+//AWARDS
+new Swiper(".awards .swiper-container", {
+  slidesPerView: 5, // 한 번에 보여줄 슬라이드 개수
+  spaceBetween: 10, // 슬라이드 사이 여백(px)
+  centeredSlides: true, // 1번 슬라이드가 가운데
+  loop: true,
+  autoplay: {
+    delay: 5000 // 5s
+  }, // 객체면 autoplay 추가 옵션 기재 가능
+  navigation: {
+    prevEl: ".awards .swiper-prev",
+    nextEl: ".awards .swiper-next"
+  }
+});
+
+//To-Top
+const toTopEl = document.querySelector(".to-top");
+
+toTopEl.addEventListener("click", function () {
+  window.scrollTo({ top: 0, behavior: "smooth" });
+});
